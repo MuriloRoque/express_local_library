a1fb63fffe285a4fb8ae52555f1ee48f45c7530d
Murilo Roque <muriloengqui@gmail.com>
2020-09-13 07:05:30 -0300

    create virtual formatted date in bookinstance model

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const { Schema } = mongoose;
 
@@ -19,6 +20,11 @@ BookInstanceSchema
   .virtual('url')
   .get(function () {
     return `/catalog/bookinstance/${this._id}`;
+  });
+BookInstanceSchema
+  .virtual('due_back_formatted')
+  .get(function () {
+    return moment(this.due_back).format('MMMM Do, YYYY');
   });
 /* eslint-enable */
 // Export model
